e9b9b937275991aafafb1f921bc2335a6d0d1b7c
AhmettK <ahmetkayaalp42@hotmail.com>
2023-01-13 18:15:52 +0300

    13 Ocak

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //Bunun adı Generic Repository Design Pattern
+    public interface IEntityRepository<T> where T:class,IEntity,new()//Generic Constraint
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);//filter=null filtresizde olabilir demek filtre vermezsek hepsini getir.
+        T Get(Expression<Func<T, bool>> filter);//Parantez içindeki LINQ aslında filtreleme yapmamızı sağlıyo, Burada null yazmadık filtre mecburi.
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+    }
+}
