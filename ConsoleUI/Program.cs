961eff4084047384539c9cfc9f783278ccb0b477
AhmettK <ahmetkayaalp42@hotmail.com>
2023-01-16 18:11:56 +0300

    10. Day Final

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -27,9 +27,17 @@ namespace ConsoleUI
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetProducDetails())
+            var result = productManager.GetProducDetails();
+            if (result.Success==true)
             {
-                Console.WriteLine(product.ProductName+"/"+product.CategoryName);
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+                }
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
             }
         }
     }
