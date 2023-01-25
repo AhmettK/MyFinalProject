6a14d55f84c58b9d5eb4db1a6c7debb748e4b9b0
AhmettK <ahmetkayaalp42@hotmail.com>
2023-01-25 16:22:21 +0300

    25 ocak

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -10,8 +11,9 @@ namespace Business.Constants
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
         public static string ProductsListed = "Ürünler listelendi";
         public static string MaintenanceTime = "Sistem bakımda";
-        public static string ProductCountOfCategoryError="Bu kategoride daha fazla ürün eklenemez";
-        public static string ProductNameAlreadyExist="Bu isimde zaten başka bir ürün var";
-        public static string CategoryLimitExceded="Kategori limiti aşıldığı için yeni ürün eklenemiyor";
+        public static string ProductCountOfCategoryError = "Bu kategoride daha fazla ürün eklenemez";
+        public static string ProductNameAlreadyExist = "Bu isimde zaten başka bir ürün var";
+        public static string CategoryLimitExceded = "Kategori limiti aşıldığı için yeni ürün eklenemiyor";
+        public static string AuthorizationDenied = "Yetkiniz yok.";
     }
 }
